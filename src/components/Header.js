3d02e38db40b1ba47dca56b0183744160ec05577
vishnu <vishnushaji797@gmail.com>
2023-12-27 06:05:41 +0530

    27/12/2023

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1>Component1</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/TODOAPP/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div className='todoContainer'>
+         <div className='header'>TODO APP</div>
+         <div className='addtask'>ADD TASK</div>
+         <div className='tasks'>LIST TASK</div>
+    </div>
+  )
+}
+
+export default Todo
